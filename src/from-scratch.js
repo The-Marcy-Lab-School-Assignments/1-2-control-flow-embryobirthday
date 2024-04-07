64c6f1f826e12542c7ea5f3fb62f926b6e2c62fc
embryobirthday <alexinverda@gmail.com>
2024-04-07 13:34:03 -0400

    progress, thank you Madhur :]

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -14,7 +14,9 @@ const measureRain = (inches) => {
   return
 };
 
-console.log(measureRain(0));
+// measureRain(3);
+// console.log(measureRain(0)); //test
+
 //
 const happyBirthdayPet = (breed, age) => {
   if (breed === "snake"){
@@ -34,36 +36,64 @@ const happyBirthdayPet = (breed, age) => {
   }
 };
 
-console.log(happyBirthdayPet("bird", 1))
+// console.log(happyBirthdayPet("bird", 1)) //test
 
+//I need to draw a diagram here because IDK where tf I went wrong
 const funTypes = (jsType) => {
-  if (typeof jsType === "string") {
+
+  // console.log(typeof jsType) //logs actual type!
+
+  if (typeof jsType === 'string'){ //typeof will never empty string, it's still a string
     console.log("That's just some text.")
-  } else if (typeof jsType === null){
-    console.log("Nothing, and I did set that." )
-  } else if (Number.isNaN(jsType)){ //NaN needs to be before the number 
+  } else if (Number.isNaN(jsType) === true) { //for some reason, NaN is counted as a number, and thus produces the "that's a good number" response. I have to check for NaN first before we get to a number condition.
     console.log("Well, now you're just showing off.")
-  } else if (typeof jsType === undefined){
-    console.log("Nothing, but I didn't set that.")
-  } else if (typeof jsType === "number"){
+  } else if (typeof jsType === 'number') {
     console.log("That's a good number.")
-  } else if (typeof jsType === "boolean"){
+  } else if (typeof jsType === 'boolean') {
     console.log("To bool, or not to bool?")
-  } else if (typeof jsType === "object"){
-    console.log("Anybody got the key?")
-  } else if (typeof jsType === "array"){
+  } else if (typeof jsType === 'undefined') {
+    console.log("Nothing, but I didn't set that.")
+  } else if (Array.isArray(jsType) === true) { //issue //
     console.log("I order you to be indexed.")
+  } else if (jsType === null) { //issue //null is an obj that "claims emptiness," you're
+    console.log("Nothing, and I did set that.")
+  } else if (typeof jsType === 'object') {
+    console.log("Anybody got the key?")
   } 
-  return "what."
 };
 
-console.log(funTypes(undefined))
+//specific condition goes before general, it's like creating a filter on a filter
 
-const rounder = () => {
+
+// console.log(funTypes(true)) //test
+
+//using the Math. methods to round up or down based on the string argument passed into roundSetting parameter 
+const rounder = (float, roundingSetting) => {
+  if (roundingSetting === "up") {
+    return Math.ceil(float)
+  } else if (roundingSetting === "down") {
+    return Math.floor(float)
+  } else if (roundingSetting === "honest") {
+    return Math.round(float)
+  } 
 };
 
-const fizzBuzzish = () => {
+// console.log(rounder(4.3, "up")); //test
+
+
+const fizzBuzzish = (num) => {
+  if (num % 3 === 0 && num % 5 !== 0) {
+    console.log("fizz")
+  } else if (num % 5 === 0 && num % 3 !== 0) {
+    console.log("buzz")
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    console.log("fizzBuzz!")
+  } else {
+    console.log(num)
+  }
 };
+
+// console.log(fizzBuzzish(12)) //test
 
 module.exports = {
   measureRain,
--- src/modify.js
+++ src/modify.js
@@ -1,4 +1,12 @@
 const wildlyBiasedReview = (location) => {
+
+  if (!location || location != 'NYC'){
+    console.log('Yea that place is cool I guess');
+    return;
+  }
+  //guard clause makes sure that there's a location to pass through, and in this case, makes sure that the 
+  // location is "NYC".
+
   if (location === 'NYC') {
     console.log('THE GREATEST CITY IN THE WORLD');
     console.log('THE CITY THAT NEVER SLEEPS');
@@ -11,3 +19,4 @@ const wildlyBiasedReview = (location) => {
 module.exports = {
   wildlyBiasedReview,
 };
+
